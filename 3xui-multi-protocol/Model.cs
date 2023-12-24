b83e36bffdb4c4cb3ec17750061f5ee8623a4f02
Mohammad <61095662+mohammad82mr@users.noreply.github.com>
2023-12-24 23:53:46 +0330

    Add project files.

---
+++ 3xui-multi-protocol/Model.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MultiProtocolContext : DbContext
+{
+    public DbSet<Inbound> Inbounds { get; set; }
+    public DbSet<Client_Traffics> Client_Traffics { get; set; }
+
+    public string DbPath { get; }
+
+    public MultiProtocolContext()
+    {
+        var folder = "/etc/x-ui/";
+        DbPath = System.IO.Path.Join(folder, "x-ui.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+
+
+public class Inbound
+{
+    public int? Id { get; set; }
+    public string? listen { get; set; }
+    public int? user_id { get; set; }
+    public Int64? Up { get; set; }
+    public Int64? Down { get; set; }
+    public Int64? Total { get; set; }
+    public string? Settings { get; set; }
+    public string? tag { get; set; }
+    public string? sniffing { get; set; }
+    public string? Stream_Settings { get; set; }
+    public string? Remark { get; set; }
+    public bool? Enable { get; set; }
+    public Int64? Expiry_Time { get; set; }
+
+    public int? Port { get; set; }
+    public string? Protocol { get; set; }
+}
+
+
+
+
+public class Client_Traffics
+{
+    public int? Id { get; set; }
+    public int? Inbound_Id { get; set; }
+    public int? Reset { get; set; }
+    public string? Email { get; set; }
+    public Int64? Up { get; set; }
+    public Int64? Down { get; set; }
+    public Int64? Total { get; set; }
+    public Int64? Expiry_Time { get; set; }
+    public bool? Enable { get; set; }
+
+}
+
+public class Client
+{
+    public string? id { get; set; }
+    public string? subId { get; set; }
+    public string? email { get; set; }
+    public string? tgId { get; set; }
+    public string? flow { get; set; }
+    public Int64? totalGB { get; set; }
+    public bool? enable { get; set; }
+    public Int64? expiryTime { get; set; }
+    public int? limitIp { get; set; }
+
+}
+
+public class inboundsetting
+{
+    public List<Client> clients { get; set; }
+    public string decryption { get; set; }
+    public List<object> fallbacks { get; set; }
+}
---
+++ 3xui-multi-protocol/Program.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+
+while (true)
+{
+    Thread.Sleep(5000);
+    using var db = new MultiProtocolContext();
+
+    var Clients = db.Client_Traffics
+        .ToList();
+
+    List<Client> ALLClients = new List<Client>();
+
+    var inbounds = db.Inbounds.ToList();
+    foreach (var item in inbounds)
+    {
+        inboundsetting setting = JsonConvert.DeserializeObject<inboundsetting>(item.Settings);
+        ALLClients.AddRange(setting.clients);
+    }
+
+    List<Client> FinalClients = new List<Client>();
+    List<Client_Traffics> FinalClients_Traffic = new List<Client_Traffics>();
+
+    foreach (var client in ALLClients)
+    {
+        if (!FinalClients.Any(x => x.subId == client.subId))
+        {
+            if (ALLClients.Where(x => x.subId == client.subId).Count() > 1)
+            {
+
+                List<Client> Calculate = ALLClients.Where(x => x.subId == client.subId).ToList();
+                List<Client_Traffics> Calculate2 = new List<Client_Traffics>();
+                foreach (var client2 in Calculate)
+                {
+                    Calculate2.Add(Clients.Where(x => x.Email == client2.email).FirstOrDefault());
+                }
+                Int64? maxTotalGB = Calculate.Max(x => x.totalGB);
+                Int64? maxTotal = Calculate2.Max(x => x.Total);
+                Int64? maxUP = Calculate2.Max(x => x.Up);
+                Int64? maxDOWN = Calculate2.Max(x => x.Down);
+                foreach (var cal in Calculate)
+                {
+                    cal.totalGB = maxTotalGB;
+                    FinalClients.Add(cal);
+                }
+                foreach (var cal2 in Calculate2)
+                {
+                    cal2.Total = maxTotal;
+                    cal2.Up = maxUP;
+                    cal2.Down = maxDOWN;
+                    FinalClients_Traffic.Add(cal2);
+                }
+            }
+
+        }
+
+    }
+
+
+    db.Client_Traffics.UpdateRange(FinalClients_Traffic);
+
+    List<Inbound> FinalInbounds = new List<Inbound>();
+    foreach (var inbound in db.Inbounds)
+    {
+
+        inboundsetting setting = JsonConvert.DeserializeObject<inboundsetting>(inbound.Settings);
+        var clis = FinalClients_Traffic.Where(x => x.Inbound_Id == inbound.Id).ToList();
+        List<Client> addtoInbound = new List<Client>();
+        foreach (var client in clis)
+        {
+            addtoInbound.Add(FinalClients.Where(x => x.email == client.Email).FirstOrDefault());
+
+        }
+        if (addtoInbound.Count() > 0)
+        {
+
+            List<Client> pastclients = new List<Client>();
+            foreach (Client client in setting.clients)
+                if (!addtoInbound.Any(x => x.email == client.email)) { pastclients.Add(client); }
+            pastclients.AddRange(addtoInbound);
+            setting.clients = pastclients;
+            inbound.Settings = JsonConvert.SerializeObject(setting);
+            FinalInbounds.Add(inbound);
+        }
+    }
+    db.Inbounds.UpdateRange(FinalInbounds);
+    db.SaveChanges();
+    Console.WriteLine("Done");
+}
+
+
+
+
+
+
