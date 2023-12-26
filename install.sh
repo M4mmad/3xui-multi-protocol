0a12b13e45b389887cf08fc806e7f606de06d5b5
Mammad <61095662+M4mmad@users.noreply.github.com>
2023-12-27 01:36:08 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
   apt install git
   git clone https://github.com/M4mmad/3xui-multi-protocol.git
+  wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+  chmod +x ./dotnet-install.sh
+  ./dotnet-install.sh --channel 7.0
+  apt update
+  apt install -y dotnet7
   cd 3xui-multi-protocol/3xui-multi-protocol
-  wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb 
-  dpkg -i packages-microsoft-prod.deb 
-  rm packages-microsoft-prod.deb 
-  apt-get update 
-  apt-get install -y dotnet-sdk-7.0 
-  apt-get install -y aspnetcore-runtime-7.0 
-  apt-get install -y dotnet-runtime-7.0 
-  apt install libc6 
   dotnet publish -c Release -o /etc/3xui-multi-protocol
   cd /etc/systemd/system/
   wget https://raw.githubusercontent.com/M4mmad/3xui-multi-protocol/master/3xui-multi-protocol.service
