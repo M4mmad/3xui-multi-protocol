b4ecf731308a467fcd7b9d8fb54d89ba7157fcb4
Mammad <61095662+M4mmad@users.noreply.github.com>
2024-01-01 21:50:11 +0330

    Update unistall.sh

--- unistall.sh
+++ unistall.sh
@@ -6,10 +6,6 @@
     systemctl reset-failed
     rm -rf /etc/3xui-multi-protocol 
     rm -rf 3xui-multi-protocol/3xui-multi-protocol 
-    apt-get purge dotnet-sdk-7.0
-    apt remove dotnet*
-    apt remove aspnetcore*
-    apt remove netstandard*
-   rm -f /etc/apt/sources.list.d/microsoft-prod.list && apt update
+
     echo "Uninstalled Successfully!"
 
